725556037fea6de8c4d3d1bce98026bb30f93b76
Edson <edson_csl@hotmail.com>
2020-04-02 16:01:36 -0300

    desafio 1

---
+++ controllers/UsuarioController.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt');
+const fs = require('fs')
+const path = require('path')
+module.exports = {
+  listUser:(req, res) => {
+      
+    res.render('cadastroUsuario', {title: 'cadastro-Usuario'})
+    
+  },
+  cadastroUsuario:(req, res) => {
+    const {nome, email} = req.body;
+    const senha = bcrypt.hashSync(req.body.senha, 10);
+    const caminho = path.join('db', 'usuarios.json')
+    let listaContato = []
+    if(fs.existsSync(caminho)){
+      listaContato = fs.readFileSync(caminho, {encoding: 'utf-8'});
+      
+      listaContato = JSON.parse(listaContato) // converte pra obj
+
+    } 
+    
+    listaContato.push({nome, email, senha})
+    
+    listaContato = JSON.stringify(listaContato);
+    
+    fs.writeFileSync(caminho, listaContato);
+    
+    
+    
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/HomeController')
+const UsuarioController = require('../controllers/UsuarioController')
+
 /* GET home page. */
 
 router.get('/', homeController.index);
 router.post('/contato', homeController.contato)
 router.post('/sucesso', homeController.newsletter)
 router.get('/listFiles', homeController.listFiles)
+
+
+router.get('/cadastro', UsuarioController.listUser )
+router.post('/cadastro', UsuarioController.cadastroUsuario )
 
 module.exports = router;
